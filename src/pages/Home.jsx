26d4c8d083dc77bd47fd47ef44b749ae56ea39cb
Ruan Pablo Pionte <ruanpablopionte@gmail.com>
2023-10-17 13:02:14 -0300

    implement  a component that shows when there is no item o localStorage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,45 @@
 import Container from "react-bootstrap/Container";
 import CardSection from "../components/CardDashboard";
 import IntensInfoSection from "../components/ItensInfoSection";
+import { useContext } from "react";
+import { stockContext } from "../contexts/StockContext";
+import { Link } from "react-router-dom";
 
 export default function Home() {
+  const { items } = useContext(stockContext);
+  const hasItems = items.length > 0;
+
   return (
     <>
-      <Container fluid>
-        <h1 className="mb-3">DashBoard</h1>
-        <CardSection />
-      </Container>
-      <IntensInfoSection />
+      {hasItems ? (
+        <Container fluid>
+          <h1 className="mb-3">DashBoard</h1>
+          <CardSection />
+          <IntensInfoSection />
+        </Container>
+      ) : (
+        <div
+          className="container text-center d-flex justify-content-center align-items-center"
+          style={{
+            height: "80vh",
+          }}
+        >
+          <div>
+            <p className="text-info display-1">Nennhum item</p>
+            <p className="text-info display-4">
+              Adicinone um item para começar
+            </p>
+            <button className="btn btn-primary ">
+              <Link
+                className="text-white text-decoration-none "
+                to={"Itens/NewItem"}
+              >
+                Adicionar item
+              </Link>
+            </button>
+          </div>
+        </div>
+      )}
     </>
   );
 }
